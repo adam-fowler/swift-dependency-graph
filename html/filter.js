3562f4e1deafc86797b1f485d22d44852ff37481
Adam Fowler <adamfowler71@gmail.com>
2019-08-21 15:55:13 +0100

    Create filter entries as and when needed

--- html/filter.js
+++ html/filter.js
@@ -1,47 +1,39 @@
 /*
  * JS Filter list
  */
-function setFilterListOptions(strings, data, onClickFunctionName) {
-    ul = document.getElementById("filterList");
-    let html = []
-    for (i=0; i < strings.length; i++) {
-        html.push(`<li style='display:none;' onclick='${onClickFunctionName}(\"${data[i]}\")'>${strings[i]}</li>`)
-    }
-    ul.innerHTML = html.join("")
+
+var filterNames = []
+var filterData = []
+var onClickFunctionName = ""
+
+function setFilterListOptions(strings, data, onClick) {
+    filterNames = strings
+    filterData = data
+    onClickFunctionName = onClick
 }
 
 function filterList() {
   // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
+  var input, filter, ul, i, txtValue;
   input = document.getElementById('filterInput');
   filter = input.value.toUpperCase();
   ul = document.getElementById("filterList");
-  li = ul.getElementsByTagName('li');
 
   if (filter.length <= 2) {
       filter = "@%@%@"
   }
+  let html = []
   // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    //a = li[i].getElementsByTagName("a")[0];
-    txtValue = li[i].textContent || li[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
+  for (i = 0; i < filterNames.length; i++) {
+      txtValue = filterNames[i]
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          html.push(`<li onclick='${onClickFunctionName}(\"${filterData[i]}\")'>${filterNames[i]}</li>`)
+      }
   }
+  ul.innerHTML = html.join("")
 }
 
 function hideFilterList() {
-    var input, ul, li, i;
-    input = document.getElementById('filterInput');
-    ul = document.getElementById("filterList");
-    li = ul.getElementsByTagName('li');
-
-    input.value = ""
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        li[i].style.display = "none";
-    }
+    var ul = document.getElementById("filterList");
+    ul.innerHTML = ""
 }
