ebdbdc3c6e236847d435bd0e05fbe780ae8d6a75
Adam Fowler <adamfowler71@gmail.com>
2019-08-23 20:41:20 +0100

    Make sure mac doesn't sleep while running

--- Sources/swift-dependency-graph/main.swift
+++ Sources/swift-dependency-graph/main.swift
@@ -1,6 +1,30 @@
 import Foundation
 import NIO
 import swift_dependency_graph_lib
+import IOKit.pwr_mgt
+
+class System {
+    /// prevent system going to sleep. Returns an id you have to supply to re-enable system sleep
+    class func preventSleep(reason:String) -> IOPMAssertionID? {
+        var assertionID = IOPMAssertionID(0)
+        let success = IOPMAssertionCreateWithName(kIOPMAssertPreventUserIdleSystemSleep as CFString,
+                                                  IOPMAssertionLevel(kIOPMAssertionLevelOn),
+                                                  reason as CFString,
+                                                  &assertionID)
+        if success == kIOReturnSuccess {
+            return assertionID
+        }
+        return nil
+    }
+    
+    /// re-enable system sleep. Pass in the id returned from the matching preventSleep() call
+    class func allowSleep(id : IOPMAssertionID) {
+        IOPMAssertionRelease(id)
+    }
+}
+
+let startTime = Date()
+let id = System.preventSleep(reason: "Swift Dependency Graph")
 
 let packages = Packages()
 let rootPath = #file.split(separator: "/", omittingEmptySubsequences: false).dropLast(3).joined(separator: "/")
@@ -11,4 +35,8 @@ do {
     try packages.save(filename: rootPath + "/dependencies.json")
 } catch {
     print(error)
+}
+print("Dependency generation took \(Int(-startTime.timeIntervalSinceNow)) seconds")
+if let id = id {
+    System.allowSleep(id: id)
 }
