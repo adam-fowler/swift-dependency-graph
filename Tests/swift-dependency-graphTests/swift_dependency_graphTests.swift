5bf9db38e41037cff6acfcbad18af6126239fed8
Adam Fowler <adamfowler71@gmail.com>
2019-08-23 16:45:14 +0100

    Added tests for common issues
    - http to https redirect
    - uppercase gitlab redirect
    - v4 manifest
    - git@github
    added commented tests I can't fix at the moment
    - v5.1 manifests
    - repositories without a master branch

--- Tests/swift-dependency-graphTests/swift_dependency_graphTests.swift
+++ Tests/swift-dependency-graphTests/swift_dependency_graphTests.swift
@@ -1,6 +1,14 @@
 import XCTest
 @testable import swift_dependency_graph_lib
 
+func attempt(function: () throws -> ()) {
+    do {
+        try function()
+    } catch {
+        XCTFail(error.localizedDescription)
+    }
+}
+
 final class swift_dependency_graphTests: XCTestCase {
     func testPackagesCleanupName() {
         XCTAssertEqual(Packages.cleanupName("https://github.com/user/repository"), "https://github.com/user/repository")
@@ -8,6 +16,48 @@ final class swift_dependency_graphTests: XCTestCase {
         XCTAssertEqual(Packages.cleanupName("https://github.com/user/repository.git"), "https://github.com/user/repository")
     }
 
+    func testLoadPackageTest() {
+        attempt {
+            try PackageLoader(addPackage: { _,_ in }).addPackage(url: "https://github.com/adam-fowler/swift-dependency-graph").wait()
+        }
+    }
+    
+    func testLoadRedirectPackageTest() {
+        attempt {
+            try PackageLoader(addPackage: { _,_ in }).addPackage(url: "http://github.com/adam-fowler/swift-dependency-graph").wait()
+        }
+    }
+    
+    func testLoadGitlabWithUppercaseLetterPackageTest() {
+        attempt {
+            try PackageLoader(addPackage: { _,_ in }).addPackage(url: "https://gitlab.com/Mordil/swift-redi-stack").wait()
+        }
+    }
+    
+    func testLoadGitAtGitHubPackageTest() {
+        attempt {
+            try PackageLoader(addPackage: { _,_ in }).addPackage(url: "git@github.com:adam-fowler/swift-dependency-graph").wait()
+        }
+    }
+    
+    func testLoadPackageV4Test() {
+        attempt {
+            try PackageLoader(addPackage: { _,_ in }).addPackage(url: "https://github.com/getguaka/env.git").wait()
+        }
+    }
+    
+    /*func testLoadPackageV5_1Test() {
+        attempt {
+            try PackageLoader(addPackage: { _,_ in }).addPackage(url: "https://github.com/freak4pc/combinecocoa.git").wait()
+        }
+    }*/
+    
+    /*func testLoadNonMasterPackageTest() {
+        attempt {
+            try PackageLoader(addPackage: { _,_ in }).addPackage(url: "https://github.com/Flinesoft/AnyMenu.git").wait()
+        }
+    }*/
+    
     static var allTests = [
         ("testPackagesCleanupName", testPackagesCleanupName),
     ]
