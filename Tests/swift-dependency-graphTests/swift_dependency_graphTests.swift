4bb8b1e06d30180702c33ec9430790e825870822
Adam Fowler <adamfowler71@gmail.com>
2019-08-23 08:11:31 +0100

    Open links in a new window

--- Tests/swift-dependency-graphTests/swift_dependency_graphTests.swift
+++ Tests/swift-dependency-graphTests/swift_dependency_graphTests.swift
@@ -3,9 +3,9 @@ import XCTest
 
 final class swift_dependency_graphTests: XCTestCase {
     func testPackagesCleanupName() {
-        XCTAssertEqual(Packages.cleanupName("https://github.com/user/repository"), "https://github.com/user/repository/")
-        XCTAssertEqual(Packages.cleanupName("https://github.com/user/repository/"), "https://github.com/user/repository/")
-        XCTAssertEqual(Packages.cleanupName("https://github.com/user/repository.git"), "https://github.com/user/repository/")
+        XCTAssertEqual(Packages.cleanupName("https://github.com/user/repository"), "https://github.com/user/repository")
+        XCTAssertEqual(Packages.cleanupName("https://github.com/user/repository/"), "https://github.com/user/repository")
+        XCTAssertEqual(Packages.cleanupName("https://github.com/user/repository.git"), "https://github.com/user/repository")
     }
 
     static var allTests = [
