d204ff888f3c9055da5f287dd914dd4767708473
Adam Fowler <adamfowler71@gmail.com>
2020-01-26 11:39:08 +0000

    Add patch and commandline options

--- Package.swift
+++ Package.swift
@@ -10,10 +10,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swift-server/async-http-client", .upToNextMajor(from: "1.0.0-alpha.1")),
-        .package(url: "https://github.com/apple/swift-package-manager", .branch("swift-5.1-DEVELOPMENT-SNAPSHOT-2020-01-23-a"))
+        .package(url: "https://github.com/apple/swift-package-manager", .branch("swift-5.1-DEVELOPMENT-SNAPSHOT-2020-01-23-a")),
+        .package(url: "https://github.com/kylef/commander", .upToNextMajor(from:"0.9.1"))
     ],
     targets: [
-        .target(name: "swift-dependency-graph", dependencies: ["swift-dependency-graph-lib"]),
+        .target(name: "swift-dependency-graph", dependencies: ["swift-dependency-graph-lib", "Commander"]),
         .target(name: "swift-dependency-graph-lib", dependencies: ["AsyncHTTPClient", "SwiftPM-auto"]),
         .testTarget(name: "swift-dependency-graphTests", dependencies: ["swift-dependency-graph-lib"]),
     ]
--- Sources/swift-dependency-graph/main.swift
+++ Sources/swift-dependency-graph/main.swift
@@ -2,6 +2,7 @@ import Foundation
 import NIO
 import swift_dependency_graph_lib
 import IOKit.pwr_mgt
+import Commander
 
 class System {
     /// prevent system going to sleep. Returns an id you have to supply to re-enable system sleep
@@ -23,20 +24,38 @@ class System {
     }
 }
 
-let startTime = Date()
-let id = System.preventSleep(reason: "Swift Dependency Graph")
+func run(_ file: String, patch: Bool) {
+    let startTime = Date()
+    let id = System.preventSleep(reason: "Swift Dependency Graph")
+
+    let url = "https://raw.githubusercontent.com/daveverwer/SwiftPMLibrary/master/packages.json"
+
+    do {
+        let packages: Packages
+        // load json that is already there
+        if patch {
+            let data = try Data(contentsOf: URL(fileURLWithPath: file))
+            let packageList = try JSONDecoder().decode(Packages.Container.self, from: data)
+            packages = try Packages(packages: packageList)
+        } else {
+            packages = try Packages()
+        }
+        try packages.import(url: url)
+        try packages.save(filename: file)
+    } catch {
+        print(error)
+    }
+    print("Dependency generation took \(Int(-startTime.timeIntervalSinceNow)) seconds")
+    if let id = id {
+        System.allowSleep(id: id)
+    }
+}
 
 let rootPath = #file.split(separator: "/", omittingEmptySubsequences: false).dropLast(3).joined(separator: "/")
-let url = "https://raw.githubusercontent.com/daveverwer/SwiftPMLibrary/master/packages.json"
 
-do {
-    let packages = try Packages()
-    try packages.import(url: url)
-    try packages.save(filename: rootPath + "/dependencies.json")
-} catch {
-    print(error)
-}
-print("Dependency generation took \(Int(-startTime.timeIntervalSinceNow)) seconds")
-if let id = id {
-    System.allowSleep(id: id)
-}
+command(
+    Option<String>("output", default: rootPath + "/dependencies.json"),
+    Flag("patch", default:true, flag:"p", description: "Patch dependencies json")
+) { path, patch in
+    run(path, patch: patch)
+}.run()
