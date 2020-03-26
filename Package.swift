50e84a279b0e7e4ff117019744990c99fbdb6f54
Adam Fowler <adamfowler71@gmail.com>
2020-03-26 11:41:10 +0000

    Updated to swift 5.1.5 release of swift-package-manager

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .executable(name: "swift-dependency-graph", targets: ["swift-dependency-graph"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-server/async-http-client", .upToNextMajor(from: "1.0.0-alpha.1")),
-        .package(url: "https://github.com/apple/swift-package-manager", .branch("swift-5.1-DEVELOPMENT-SNAPSHOT-2020-01-23-a")),
+        .package(url: "https://github.com/swift-server/async-http-client", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/apple/swift-package-manager", .branch("swift-5.1.5-RELEASE")),
         .package(url: "https://github.com/kylef/commander", .upToNextMajor(from:"0.9.1"))
     ],
     targets: [
